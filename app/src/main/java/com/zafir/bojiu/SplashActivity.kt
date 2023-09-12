45b399a873384f36350eb01ea6f8eb9b0aa415cd
wuzhen6 <wuzhen6@360.cn>
2023-09-12 20:06:00 +0800

    【feature】 修改style

--- app/src/main/java/com/zafir/bojiu/SplashActivity.kt
+++ app/src/main/java/com/zafir/bojiu/SplashActivity.kt
@@ -1,11 +1,13 @@
 package com.zafir.bojiu
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
